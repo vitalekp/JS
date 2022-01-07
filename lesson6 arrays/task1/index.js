5f877e004bca94ed8744b47d0f6987486392e18a
vitalekp <vitalekpushkarov@gmail.com>
2022-01-07 11:34:33 +0200

    task1

---
+++ lesson6 arrays/task1/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function getArrayBounds(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  return [arr.length, arr[0], arr[arr.length - 1]];
+}
+
+// examples
+getArrayBounds([1, 10, 9, 11]); // ==> [4, 1, 11]
+getArrayBounds(10, 12, 14); // ==> null
+getArrayBounds([1]); // ==> [1, 1, 1]
