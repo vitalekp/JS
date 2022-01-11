d7084467c914ab78c209f37508de2d4ab4dda5bb
vitalekp <vitalekpushkarov@gmail.com>
2022-01-11 15:19:08 +0200

    task9

---
+++ lesson7 arrays_methods/task9/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const withdraw = (clients, balances, client, amount) => {
+const indexClient = clients.indexOf(client);
+
+  if(amount > balances[indexClient]) {
+    return -1;
+  } 
+
+  return balances[indexClient] - amount;
+}
+
+
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50));
+
+
+//example 1:
+
+//input
+withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50)
+
+//output
+//37
+//и массив balances должен быть [1400, 37, -6]
+
+
+
+//example 2:
+
+//input
+// withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10)
+
+//output
+//-1
+//и массив balances должен быть [1400, 87, -6]
