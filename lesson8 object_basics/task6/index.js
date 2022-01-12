6aab24c9ec4fcba6272cdeea51082aca70ceafcd
vitalekp <vitalekpushkarov@gmail.com>
2022-01-12 12:31:56 +0200

    task6

---
+++ lesson8 object_basics/task6/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-restricted-syntax */
+
+/* Для решения этой задачи используй for..in цикл. В реальных проектах это плохая практика,
+ * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
+ * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
+ * */
+
+const pickProps = (obj, props) => {
+  let objProps = {};
+
+  for (let key in obj) {
+     if(props.includes(key)) {
+       objProps[key] = obj[key]
+     }
+  }
+  console.log(objProps);
+  return objProps;
+};
+
+// examples
+pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c']); // ==> { a: 1, c: 3 }
+pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c', 'd', 'hex']); // ==> { a: 1, c: 3 }
