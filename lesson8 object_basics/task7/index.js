704cc02c0aaec7b99cceaad0096c9abd0d2a4589
vitalekp <vitalekpushkarov@gmail.com>
2022-01-12 12:39:21 +0200

    task7

---
+++ lesson8 object_basics/task7/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-restricted-syntax */
+
+/* Для решения этой задачи используй for..in цикл. В реальных проектах это плохая практика,
+ * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
+ * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
+ * */
+
+const getAdults = obj => {
+  const objAdults = {};
+  const TOO_YONG = 17;
+
+  for (let key in obj) {
+    if (obj[key] > TOO_YONG) {
+      objAdults[key] = obj[key];
+    }
+  }
+
+  console.log(objAdults);
+  return objAdults;
+};
+
+// examples
+getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 }); // ==> { 'John Doe': 19, Bob: 18 }
+getAdults({ Ann: 56, Andrey: 7 }); // ==> { Ann: 56 }
