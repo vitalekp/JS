7e9bdd4a05ac6a951a00874a66bf648570d27242
vitalekp <vitalekpushkarov@gmail.com>
2022-01-14 12:49:59 +0200

    task7

---
+++ lesson9 object/task7/index.js
@@ -0,0 +1,12 @@
+const getTotalRevenue = transactions => Object.values(transactions)
+    .reduce((acc, sum) => acc += sum.amount, 0);
+
+// examples
+const dayTransactions = [
+  { userId: 22, amount: 60, operation: 'sell' },
+  { userId: 22, amount: 160, operation: 'buy' },
+  { userId: 44, amount: 90, operation: 'sell' },
+];
+
+const result = getTotalRevenue(dayTransactions); // ==> 310
+console.log(result);
