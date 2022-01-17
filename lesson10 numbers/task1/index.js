dd1edac12bc3934f4a086d248d6ef6c513430334
vitalekp <vitalekpushkarov@gmail.com>
2022-01-17 11:46:52 +0200

    task1

---
+++ lesson10 numbers/task1/index.js
@@ -0,0 +1,13 @@
+'use strict';
+const multiRound = (num) => [
+  Math.floor(num * 100) / 100,
+  Math.trunc(num * 100) / 100,
+  Math.ceil(num * 100) / 100,
+  Math.round(num * 100) / 100,
+  +num.toFixed(2),
+];
+
+// examples
+multiRound(11.12556); // ==> [11.12, 11.12, 11.13, 11.13, 11.13]
+multiRound(6.112); // ==> [6.11, 6.11, 6.12, 6.11, 6.11]
+console.log(multiRound(11.12556));
