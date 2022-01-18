079241fe387e168b620caaa9403dc11e61362fd3
vitalekp <vitalekpushkarov@gmail.com>
2022-01-18 11:06:27 +0200

    task7

---
+++ lesson10 numbers/task7/index.js
@@ -0,0 +1,9 @@
+const getParsedIntegers = arr => arr.map(el => Number.parseInt(el));
+
+const getParsedIntegersV2 = arr => arr.map(el => parseInt(el));
+
+const getParsedFloats = arr => arr.map(el => Number.parseFloat(el));
+
+const getParsedFloatsV2 = arr => arr.map(el => parseFloat(el));
+
+console.log(getParsedFloatsV2([undefined, 2, 3.457]));
