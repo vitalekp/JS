d107707fd9875330aee6f82793eb20a318596f51
vitalekp <vitalekpushkarov@gmail.com>
2022-02-02 11:51:31 +0200

    task1

--- lesson21/task1/dex.js
+++ lesson21/task1/dex.js
@@ -1,7 +1,7 @@
 'use strict';
 
 export const tasks = [
-  { text: 'Buy milk', done: false },
+  { text: 'Buy milk', done: true },
   { text: 'Pick up Tom from airport', done: false },
   { text: 'Visit party', done: false },
   { text: 'Visit doctor', done: true },
