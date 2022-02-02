276f3b845a5bde15da90cb97911c37273f3d0ffa
vitalekp <vitalekpushkarov@gmail.com>
2022-02-02 12:57:26 +0200

    task3

--- lesson21/task3/index.js
+++ lesson21/task3/index.js
@@ -1,11 +1,11 @@
-const getItemsList = () => {
+export const getItemsList = () => {
   const technologyLists = document.querySelectorAll('.technology');
   console.dir(technologyLists);
 
   return technologyLists;
 };
 
-const getItemsArray = () => {
+export const getItemsArray = () => {
   const toolLists = document.querySelectorAll('.tool');
   const arr = [];
   Array.from(toolLists).map(el => arr.push(el));
