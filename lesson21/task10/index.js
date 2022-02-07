f404bc9376f20e5e60f3badfc0a995f7003cec13
vitalekp <vitalekpushkarov@gmail.com>
2022-02-07 09:15:48 +0200

    task10

--- lesson21/task10/index.js
+++ lesson21/task10/index.js
@@ -1,6 +1,9 @@
 export const finishForm = () => {
   const password = document.querySelector('input');
   password.setAttribute('type', 'password');
+  
+  const loginInput = document.createElement('input');
+  document.querySelector('.login-form').prepend(loginInput);
 };
 
-// finishForm();
+finishForm();
