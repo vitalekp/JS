42501e44686dc684bbcc1592a086ab05e19cb876
vitalekp <vitalekpushkarov@gmail.com>
2022-02-07 09:37:49 +0200

    task10

--- lesson21/task10/index.js
+++ lesson21/task10/index.js
@@ -1,10 +1,10 @@
 export const finishForm = () => {
-  const password = document.querySelector('input');
-  password.setAttribute('type', 'password');
-  password.setAttribute('name', 'login');
-
   const loginInput = document.createElement('input');
   document.querySelector('.login-form').append(loginInput);
+  loginInput.setAttribute('name', 'login');
+
+  const password = document.querySelector('input[name=login]');
+  password.setAttribute('type', 'password');
 };
 
 finishForm();
