b013c9d3d9756c33e608a7879ee5bdfde92c17a3
vitalekp <vitalekpushkarov@gmail.com>
2022-02-15 16:37:23 +0200

    task1

--- lesson8 object_basics/task1/index.js
+++ lesson8 object_basics/task1/index.js
@@ -1,13 +1,10 @@
-const transformToObject = arr => {
-  let objTransform = {};
-
-  for (let keys in arr) {
-    objTransform[arr[keys]] = arr[keys];
+const transformToObject = (arr) => {
+  const obj = {};
+  for (let key of arr) {
+    obj[key] = key;
   }
-
-  return objTransform;
+  return obj;
 };
 
 // examples
-transformToObject(["a", 17.1, "John Doe"]); // ==> { a: 'a', '17.1': 17.1, 'John Doe': 'John Doe' }
-
+transformToObject(['a', 17.1, 'John Doe']); // ==> { a: 'a', '17.1': 17.1, 'John Doe': 'John Doe' }
