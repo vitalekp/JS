21926c3f893f8d7e900c59f6b149e0fbc4668d83
vitalekp <vitalekpushkarov@gmail.com>
2022-02-17 17:04:38 +0200

    task2

--- lesson13/task2/calculator.test.js
+++ lesson13/task2/calculator.test.js
@@ -1,19 +1,19 @@
-import getSum, { getSquaredArray, getOddNumbers} from './calculator.js';
+import getSum, { getSquaredArray, getOddNumbers } from './calculator.js';
 
 it('should get squared numbers', () => {
   const result = getSquaredArray([1, 2, 3]);
-  
+
   expect(result).toEqual([1, 4, 9]);
 });
 
 it('should get odd numbers only', () => {
   const result = getOddNumbers([1, 2, 3, 4]);
-  
+
   expect(result).toEqual([1, 3]);
 });
 
 it('should get sum of numbers', () => {
   const result = getSum(10, 110);
-  
+
   expect(result).toEqual(120);
 });
