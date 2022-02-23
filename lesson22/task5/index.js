e08fd4714417e31fa7f7a0b7984179991c25ed14
vitalekp <vitalekpushkarov@gmail.com>
2022-02-23 11:34:33 +0200

    task5

--- lesson22/task5/index.js
+++ lesson22/task5/index.js
@@ -3,6 +3,6 @@
 // 2 при клике на 'search__btn'- con.log(текс из инпута 'search__input')
 
 const searchBtn = document.querySelector('.search__btn');
-searchBtn.addEventListener('click', () => {
-  console.log(document.querySelector('.search__input').value);
-});
+searchBtn.addEventListener('click', () =>
+  console.log(document.querySelector('.search__input').value)
+);
