eebed611b8b18e0e4887b9b992cc1e6acab63d65
vitalekp <vitalekpushkarov@gmail.com>
2022-02-23 10:54:41 +0200

    task2

---
+++ lesson22/task2/index.js
@@ -0,0 +1,8 @@
+// algo
+// 1 повесить обработчик 'click', с классом .create-btn +++
+// 2 при клике вывести в alert - 'done' +++
+
+const createBtnElem = document.querySelector('.create-btn');
+createBtnElem.addEventListener('click', () => {
+  alert('done');
+});
