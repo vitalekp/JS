b732e785ed817497370435bf3c9d4ae1b8038900
vitalekp <vitalekpushkarov@gmail.com>
2022-02-23 18:43:01 +0200

    task8

--- lesson22/task8/index.js
+++ lesson22/task8/index.js
@@ -3,6 +3,4 @@
 // 2 вывести при вводе текст из инпута в консоль
 
 const inputElem = document.querySelector('.text-input');
-inputElem.addEventListener('change', (event) =>
-  console.log(event.target.value)
-);
+inputElem.addEventListener('change', event => console.log(event.target.value));
