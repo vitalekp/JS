538325cec9b362208a16d46e868310e4b5abae3a
vitalekp <vitalekpushkarov@gmail.com>
2022-02-23 13:25:11 +0200

    task8

---
+++ lesson22/task8/index.js
@@ -0,0 +1,8 @@
+// algo
+// 1 обработчик 'change' на инпут '.text-input' +++
+// 2 вывести при вводе текст из инпута в консоль
+
+const inputElem = document.querySelector('.text-input');
+inputElem.addEventListener('change', (event) =>
+  console.log(event.target.value)
+);
