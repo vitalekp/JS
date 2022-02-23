54fc64d0152d48f4df0e3fa11ae9f668b156c558
vitalekp <vitalekpushkarov@gmail.com>
2022-02-23 11:33:09 +0200

    task5

---
+++ lesson22/task5/index.js
@@ -0,0 +1,8 @@
+// algo
+// 1 обработчик события класу 'search__btn'
+// 2 при клике на 'search__btn'- con.log(текс из инпута 'search__input')
+
+const searchBtn = document.querySelector('.search__btn');
+searchBtn.addEventListener('click', () => {
+  console.log(document.querySelector('.search__input').value);
+});
