8e05dc5727ebd689c49063cdc98ea3c76d8626fa
vitalekp <vitalekpushkarov@gmail.com>
2022-04-19 14:29:36 +0300

    get different date

---
+++ lesson24/task3/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// input: Date, Date
+// out: string
+
+// algo
+// 1 найти більшу дату (sort)
+// 2 відняти роки і залишок додати до днів
+// 3 відняти дні, год, хв, сек між собою
+// 4 вивести рядок
+
+export const getDiff = (startDate, endDate) => {
+  const diffTime = Math.abs(startDate - endDate);
+
+  const days = 1000 * 60 * 60 * 24;
+  const hours = days / 24;
+  const minutes = hours / 60;
+  const seconds = minutes / 60;
+
+  const dayDiff = Math.trunc(diffTime / days);
+  const hourDiff = Math.trunc((diffTime % days) / hours);
+  const minuteDiff = Math.trunc((diffTime % hours) / minutes);
+  const secondDiff = Math.trunc((diffTime % minutes) / seconds);
+
+  return `${dayDiff}d ${hourDiff}h ${minuteDiff}m ${secondDiff}s`;
+};
+
+// test data
+
+const test = getDiff(new Date(), new Date(2000, 10));
+const test2 = getDiff(new Date(), new Date(2021, 4, 19));
+
+console.log(test);
+console.log(test2);
