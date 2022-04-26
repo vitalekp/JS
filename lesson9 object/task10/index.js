b30b2bedfcb913cf73a183dee72ab6734b686f5d
vitalekp <vitalekpushkarov@gmail.com>
2022-04-26 18:06:14 +0300

    task10 compare obj

--- lesson9 object/task10/index.js
+++ lesson9 object/task10/index.js
@@ -1,14 +1,15 @@
+'use strict';
+
+// input: obj, obj
+// output: boolean
 function compareObjects(obj1, obj2) {
-  if (!(Object.keys(obj1).length === Object.keys(obj2).length)) {
+  const objKeys = Object.keys(obj1);
+
+  if (objKeys.length !== Object.keys(obj2).length) {
     return false;
   }
 
-  const key = Object.keys(obj1).join('');
-  const key2 = Object.keys(obj2).join('');
-  const value = Object.values(obj1).join('');
-  const value2 = Object.values(obj2).join('');
-
-  return key === key2 && value === value2;
+  return objKeys.every(key => obj1[key] === obj2[key]);
 }
 
 // examples
@@ -42,25 +43,11 @@ console.log(res);
 //   if (!(Object.keys(obj1).length === Object.keys(obj2).length)) {
 //     return false;
 //   }
-//   const key = Object.keys(obj1);
-//   const key2 = Object.keys(obj2);
-//   const value = Object.values(obj1);
-//   const value2 = Object.values(obj2);
-//   let checkKey = true;
-//   let checkValue = true;
 
-//   for (let char of key) {
-//     if (key2.includes(char)) {
-//       continue;
-//     }
-//     return false;
-//   }
+//   const key = Object.keys(obj1).join('');
+//   const key2 = Object.keys(obj2).join('');
+//   const value = Object.values(obj1).join('');
+//   const value2 = Object.values(obj2).join('');
 
-//   for (let char of value) {
-//     if (value2.includes(char)) {
-//       continue;
-//     }
-//     return false;
-//   }
-//   return checkKey && checkValue;
+//   return key === key2 && value === value2;
 // }
