027a98072da37a3b20fb5854d06e3d1400c71380
vitalekp <vitalekpushkarov@gmail.com>
2022-04-27 09:51:35 +0300

    task5 HM

--- lesson28 func_adv/task5/index.js
+++ lesson28 func_adv/task5/index.js
@@ -9,6 +9,61 @@
 // 2.1 до цих методів добавити можливість працювати з роками, днями, год і т.д. +++
 // 3 добавити метод result, який повертає дату після змін +++
 
+const getDateValue = {
+  years: 'getFullYear',
+  months: 'getMonth',
+  days: 'getDate',
+  hours: 'getHours',
+  minutes: 'getMinutes',
+  seconds: 'getSeconds',
+  milliseconds: 'getMilliseconds',
+};
+const setDateValue = {
+  years: 'setFullYear',
+  months: 'setMonth',
+  days: 'setDate',
+  hours: 'setHours',
+  minutes: 'setMinutes',
+  seconds: 'setSeconds',
+  milliseconds: 'setMilliseconds',
+};
+
+export const shmoment = date => {
+  const newFullDate = new Date(date);
+
+  const editDate = {
+    add(type, value) {
+      newFullDate[setDateValue[type]](newFullDate[getDateValue[type]]() + value);
+      return this;
+    },
+    subtract(type, value) {
+      newFullDate[setDateValue[type]](newFullDate[getDateValue[type]]() - value);
+      return this;
+    },
+    result() {
+      return newFullDate;
+    },
+  };
+
+  return editDate;
+};
+//  newFullDate.setFullYear(newFullDate.getFullYear() - value);
+// test data
+
+const date = new Date(2020, 0, 7, 17, 17, 17);
+const test = shmoment(new Date(date))
+  .add('years', 200)
+  .subtract('years', 100)
+  .subtract('years', 100)
+  .subtract('months', 2)
+  .subtract('minutes', 2)
+  .add('minutes', 120)
+  .result();
+
+// const dateInFuture = new Date(date).setDate(day + days);
+
+console.log(test);
+
 // const shmoment = date => {
 //   const newFullDate = new Date(date);
 
@@ -68,58 +123,3 @@
 
 //   return editDate;
 // };
-
-const getDateValue = {
-  years: 'getFullYear',
-  months: 'getMonth',
-  days: 'getDate',
-  hours: 'getHours',
-  minutes: 'getMinutes',
-  seconds: 'getSeconds',
-  milliseconds: 'getMilliseconds',
-};
-const setDateValue = {
-  years: 'setFullYear',
-  months: 'setMonth',
-  days: 'setDate',
-  hours: 'setHours',
-  minutes: 'setMinutes',
-  seconds: 'setSeconds',
-  milliseconds: 'setMilliseconds',
-};
-
-export const shmoment = date => {
-  const newFullDate = new Date(date);
-
-  const editDate = {
-    add(type, value) {
-      newFullDate[setDateValue[type]](newFullDate[getDateValue[type]]() + value);
-      return this;
-    },
-    subtract(type, value) {
-      newFullDate[setDateValue[type]](newFullDate[getDateValue[type]]() - value);
-      return this;
-    },
-    result() {
-      return newFullDate;
-    },
-  };
-
-  return editDate;
-};
-//  newFullDate.setFullYear(newFullDate.getFullYear() - value);
-// test data
-
-const date = new Date(2020, 0, 7, 17, 17, 17);
-const test = shmoment(new Date(date))
-  .add('years', 200)
-  .subtract('years', 100)
-  .subtract('years', 100)
-  .subtract('months', 2)
-  .subtract('minutes', 2)
-  .add('minutes', 120)
-  .result();
-
-// const dateInFuture = new Date(date).setDate(day + days);
-
-console.log(test);
