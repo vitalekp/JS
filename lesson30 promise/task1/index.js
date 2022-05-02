3f32bee21d1b821929f4e200d3c415b40eba73b7
vitalekp <vitalekpushkarov@gmail.com>
2022-05-02 10:42:10 +0300

    task1

---
+++ lesson30 promise/task1/index.js
@@ -0,0 +1,36 @@
+// input: string
+// output: object
+
+const addImage = imgSrc => {
+  const promise = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My photo');
+    imgElem.src = imgSrc;
+
+    const divElem = document.querySelector('.page');
+    divElem.append(imgElem);
+
+    const onImgLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+
+    imgElem.addEventListener('load', onImgLoaded);
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
+  });
+
+  console.log(promise);
+
+  return promise;
+};
+
+// examples
+const imgSrc =
+  'https://p.bigstockphoto.om/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+const resultPromise = addImage(imgSrc);
+
+resultPromise.then(data => {
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${data.width} x ${data.height}`;
+});
+resultPromise.catch(error => console.log(error));
