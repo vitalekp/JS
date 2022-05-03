48e1ae7a2863276710c7006f24cdc39b51e7e6d8
vitalekp <vitalekpushkarov@gmail.com>
2022-05-03 11:52:17 +0300

    sum promises

--- lesson32 promise_methods/task1/index.js
+++ lesson32 promise_methods/task1/index.js
@@ -5,12 +5,12 @@
 // 1 create func asyncSum() +++
 // 2 calculate the sum of all promises +++
 // 3 discard all values that are not converted to a number +++
-// 4 reject a promise with an error 'Can't calculate\'
+// 4 reject a promise with an error 'Can't calculate\' +++
 // if at least one rejected promise is received as input
 // 5 return promise +++
 
 export const asyncSum = (...asyncSum) => {
-  return Promise.all(asyncSum).then(numbers =>
-    numbers.filter(num => !isNaN(num)).reduce((acc, num) => acc + Number(num), 0),
-  );
+  return Promise.all(asyncSum)
+    .then(numbers => numbers.filter(num => !isNaN(num)).reduce((acc, num) => acc + Number(num), 0))
+    .catch(() => Promise.reject(new Error(`Can't calculated`)));
 };
