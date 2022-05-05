d8111d5057bc7bd46fc96efb0a4b1f092adf59ff
vitalekp <vitalekpushkarov@gmail.com>
2022-05-05 16:52:36 +0300

    rest api todo list

---
+++ lesson34/task1/src/scripts/updateTask.js
@@ -0,0 +1,20 @@
+import { renderTasks } from './renderer.js';
+import { getItem, setItem } from './storage.js';
+
+export const onToggleTask = e => {
+  const tasksList = getItem('tasksList');
+  const newTasksList = tasksList.map(task => {
+    if (task.id === e.target.dataset.id) {
+      const done = e.target.checked;
+      return {
+        ...task,
+        done,
+        finishDate: done ? new Date().toISOString() : null,
+      };
+    }
+    return task;
+  });
+  setItem('tasksList', newTasksList);
+
+  renderTasks();
+};
