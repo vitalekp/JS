d45968f4417c4759f7cfd21d6621a705ca87ab6b
vitalekp <vitalekpushkarov@gmail.com>
2022-05-11 12:49:38 +0300

    task1

---
+++ lesson36-async-await/task1/index.js
@@ -0,0 +1,16 @@
+export const fetchUser = async userId => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    if (!response.ok) {
+      return null;
+    }
+    const userData = await response.json();
+    return userData;
+  } catch (err) {
+    throw new Error('Failed to get user data');
+  }
+};
+
+fetchUser('facebook')
+  .then(userData => console.log(userData))
+  .catch(err => alert(err));
