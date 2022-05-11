710aeb0c7c9cf40d19b44f9bcf0c8e009673001b
vitalekp <vitalekpushkarov@gmail.com>
2022-05-11 13:22:22 +0300

    todo list async await

---
+++ lesson36-async-await/task2/spinner.js
@@ -0,0 +1,8 @@
+const spinnerElem = document.querySelector('.spinner');
+
+export const showSpinner = () => {
+  spinnerElem.classList.remove('spinner_hidden');
+};
+export const hideSpinner = () => {
+  spinnerElem.classList.add('spinner_hidden');
+};
